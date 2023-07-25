da4c67bf0b406183e137620d301f497dfc46446e
Michał Klich <michal@klichx.dev>
2023-07-25 22:18:04 +0200

    Pass worker into supervisor

--- coroutines1.py
+++ coroutines1.py
@@ -25,7 +25,11 @@ MAX_RETRIES = 3
 
 
 async def supervisor(
-    url: str, name: str, client: aiohttp.ClientSession, retry: int = 0
+    worker,
+    url: str,
+    name: str,
+    client: aiohttp.ClientSession,
+    retry: int = 0,
 ) -> int:
     try:
         return await worker(url, name, client)
@@ -35,7 +39,7 @@ async def supervisor(
             # jitter
             await asyncio.sleep(random.random())
             logging.warning("Retrying coroutine %s. Retry: %s", name, retry)
-            return await supervisor(url, name, client, retry)
+            return await supervisor(worker, url, name, client, retry)
 
         logging.warning("Retries exhausted for call args %s", (url, client, retry))
         return 0
@@ -47,7 +51,7 @@ async def supervisor(
 
 # tenacity can be used here
 async def worker(url: str, name: str, session: aiohttp.ClientSession) -> int:
-    bad_luck = random.random()
+    bad_luck: float = random.random()
     if bad_luck > 0.9:
         logging.warning("Status: failed, name: %s, bad luck: %s", name, bad_luck)
         raise random.choice(
@@ -64,7 +68,10 @@ async def worker(url: str, name: str, session: aiohttp.ClientSession) -> int:
 async def main():
     async with aiohttp.ClientSession() as client:
         start = time.perf_counter()
-        coros = [supervisor("https://klichx.dev", str(i), client) for i in range(0, 20)]
+        coros = [
+            supervisor(worker, "https://klichx.dev", str(i), client)
+            for i in range(0, 20)
+        ]
         try:
             res = await asyncio.gather(
                 *coros,
@@ -85,20 +92,4 @@ async def main():
         logging.info("Result count %s, and items: %s", len(res), res)
 
 
-def handle_exception(loop, context):
-    # context["message"] will always be there; but context["exception"] may not
-    print("yo")
-    msg = context.get("exception", context["message"])
-    logging.error(f"Caught exception: {msg}")
-    logging.info("Shutting down...")
-
-
-loop = asyncio.get_event_loop()
-loop.set_exception_handler(handle_exception)
-task = loop.create_task(main())
-# loop.call_later(1, task.cancel)
-loop.run_until_complete(task)
-# try:
-#     asyncio.run(main())
-# except KeyboardInterrupt:
-#     logging.info("User cancelled.")
+asyncio.run(main())
